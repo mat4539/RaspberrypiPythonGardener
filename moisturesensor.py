584662d55a2ba6635e649abe4ddcca9288ae59e0
mat4539 <mborg0020@gmail.com>
2020-05-08 20:26:49 -0400

    more tests

--- moisturesensor.py
+++ moisturesensor.py
@@ -7,7 +7,7 @@ channel = 21
 moisture = 0
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
- 
+
 def callback(channel):
         if GPIO.input(channel):
                 print ("Water Detected!")
@@ -18,7 +18,8 @@ def callback(channel):
  
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
- 
+
+print (moisture)
 # infinite loop
 while True:
         time.sleep(1)
