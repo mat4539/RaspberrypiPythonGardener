3b92298d83114a4459e5d2eab9a16a2aa581f6f6
mat4539 <mborg0020@gmail.com>
2020-05-08 20:27:51 -0400

    test

--- moisturesensor.py
+++ moisturesensor.py
@@ -19,7 +19,8 @@ def callback(channel):
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
 
-print (moisture)
+
 # infinite loop
 while True:
         time.sleep(1)
+        print (moisture)
