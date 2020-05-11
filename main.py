32b74e7968cbbb52c07972588e48f2af529c67be
mat4539 <mborg0020@gmail.com>
2020-05-11 18:24:24 -0400

    if and else

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import moisturesensor
 while True:
     moisture = moisturesensor.check_water()
     print(moisture)
+    if moisture==0:
+        print("No Water Needed")
+    else:
+        print("Water Needed")
+    
