0c9294eacbd5d94e3057d211e6955978fc647eb4
Kaden S <89apt89@protonmail.ch>
2020-05-11 11:58:37 -0400

    Remove infinite loop, remove arg from channel method

--- moisturesensor.py
+++ moisturesensor.py
@@ -7,13 +7,7 @@ channel = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
-def callback(channel):
+def callback():
  
         GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
         GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
-
-
-# infinite loop
-while True:
-        time.sleep(1)
-        print(channel)
